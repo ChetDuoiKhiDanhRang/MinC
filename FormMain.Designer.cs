9ca2195a3b8b27c44d5df97760c1f519fd3f5ebd
Nguyễn Ngọc Cường <58522857+ChetDuoiKhiDanhRang@users.noreply.github.com>
2020-01-30 13:20:58 +0700

    m

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -28,30 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnBankStability = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(403, 222);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // btnBankStability
             // 
             this.btnBankStability.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.btnBankStability.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBankStability.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBankStability.Location = new System.Drawing.Point(12, 228);
+            this.btnBankStability.Location = new System.Drawing.Point(12, 141);
             this.btnBankStability.Name = "btnBankStability";
-            this.btnBankStability.Size = new System.Drawing.Size(188, 51);
+            this.btnBankStability.Size = new System.Drawing.Size(164, 51);
             this.btnBankStability.TabIndex = 1;
             this.btnBankStability.Text = "Bank stability";
             this.btnBankStability.UseVisualStyleBackColor = true;
@@ -62,19 +52,30 @@
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(203, 228);
+            this.button2.Location = new System.Drawing.Point(192, 141);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(188, 51);
+            this.button2.Size = new System.Drawing.Size(164, 51);
             this.button2.TabIndex = 1;
             this.button2.Text = "Influence of technology\r\non asset pricing";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::MinC.Properties.Resources.b;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(403, 287);
+            this.ClientSize = new System.Drawing.Size(368, 199);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnBankStability);
             this.Controls.Add(this.pictureBox1);
