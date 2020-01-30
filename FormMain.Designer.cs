266134e44629e16e15f5ca885d4cc99ae2d6ef10
Nguyễn Ngọc Cường <58522857+ChetDuoiKhiDanhRang@users.noreply.github.com>
2020-01-30 12:49:48 +0700

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MinC
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBankStability = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBankStability
+            // 
+            this.btnBankStability.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnBankStability.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBankStability.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBankStability.Location = new System.Drawing.Point(12, 228);
+            this.btnBankStability.Name = "btnBankStability";
+            this.btnBankStability.Size = new System.Drawing.Size(188, 51);
+            this.btnBankStability.TabIndex = 1;
+            this.btnBankStability.Text = "Bank stability";
+            this.btnBankStability.UseVisualStyleBackColor = true;
+            this.btnBankStability.Click += new System.EventHandler(this.btnBankStability_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(203, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Influence of technology\r\non asset pricing";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(403, 287);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnBankStability);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "FormMain";
+            this.Text = "MinC (p)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBankStability;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinC
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        FormBankStabibity frmBankStability;
+        private void btnBankStability_Click(object sender, EventArgs e)
+        {
+            if (frmBankStability==null||frmBankStability.IsDisposed)
+            {
+                frmBankStability = new FormBankStabibity();
+                frmBankStability.Show();
+            }
+        }
+    }
+}
