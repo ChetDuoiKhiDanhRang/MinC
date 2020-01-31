388bee021ed05ddf3b827f84e60fc014f321fbd2
Nguyễn Ngọc Cường <58522857+ChetDuoiKhiDanhRang@users.noreply.github.com>
2020-01-31 15:38:59 +0700

    h

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -28,36 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnBankStability = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnSelectData = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnBankStability
+            // btnSelectData
             // 
-            this.btnBankStability.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnBankStability.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBankStability.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBankStability.Location = new System.Drawing.Point(192, 141);
-            this.btnBankStability.Name = "btnBankStability";
-            this.btnBankStability.Size = new System.Drawing.Size(164, 51);
-            this.btnBankStability.TabIndex = 1;
-            this.btnBankStability.Text = "Bank stability";
-            this.btnBankStability.UseVisualStyleBackColor = true;
-            this.btnBankStability.Click += new System.EventHandler(this.btnBankStability_Click);
+            this.btnSelectData.FlatAppearance.BorderSize = 0;
+            this.btnSelectData.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSelectData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectData.Location = new System.Drawing.Point(12, 62);
+            this.btnSelectData.Name = "btnSelectData";
+            this.btnSelectData.Size = new System.Drawing.Size(106, 23);
+            this.btnSelectData.TabIndex = 1;
+            this.btnSelectData.Text = "Select data file:";
+            this.btnSelectData.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // textBox1
             // 
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Candara", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 141);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(164, 51);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Influence of technology\r\non asset pricing";
-            this.button2.UseVisualStyleBackColor = true;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(130, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(376, 20);
+            this.textBox1.TabIndex = 2;
             // 
             // pictureBox1
             // 
@@ -65,7 +62,7 @@
             this.pictureBox1.Image = global::MinC.Properties.Resources.b;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 135);
+            this.pictureBox1.Size = new System.Drawing.Size(518, 58);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -75,9 +72,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(368, 199);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.btnBankStability);
+            this.ClientSize = new System.Drawing.Size(518, 199);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSelectData);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -86,14 +83,14 @@
             this.Text = "MinC (p)";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button btnSelectData;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnBankStability;
-        private System.Windows.Forms.Button button2;
     }
 }
 
