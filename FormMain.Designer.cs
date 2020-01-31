dd2354f8ee4073062dda86ea73806c7e7c6259e8
Nguyễn Ngọc Cường <58522857+ChetDuoiKhiDanhRang@users.noreply.github.com>
2020-01-31 16:17:39 +0700

    datafile

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -31,6 +31,7 @@
             this.btnSelectData = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,17 +41,18 @@
             this.btnSelectData.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btnSelectData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSelectData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSelectData.Location = new System.Drawing.Point(12, 62);
+            this.btnSelectData.Location = new System.Drawing.Point(12, 90);
             this.btnSelectData.Name = "btnSelectData";
             this.btnSelectData.Size = new System.Drawing.Size(106, 23);
             this.btnSelectData.TabIndex = 1;
-            this.btnSelectData.Text = "Select data file:";
+            this.btnSelectData.Text = "Data file:";
             this.btnSelectData.UseVisualStyleBackColor = true;
+            this.btnSelectData.Click += new System.EventHandler(this.btnSelectData_Click);
             // 
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(130, 65);
+            this.textBox1.Location = new System.Drawing.Point(130, 93);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(376, 20);
@@ -58,14 +60,24 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = global::MinC.Properties.Resources.b;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(518, 58);
+            this.pictureBox1.Size = new System.Drawing.Size(494, 87);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 9);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
             // 
             // FormMain
             // 
@@ -73,6 +85,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(518, 199);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnSelectData);
             this.Controls.Add(this.pictureBox1);
@@ -91,6 +104,7 @@
         private System.Windows.Forms.Button btnSelectData;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -7,23 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Packaging;
 
 namespace MinC
 {
     public partial class FormMain : Form
     {
+        public event EventHandler<string> DataFileChanged;
         public FormMain()
         {
             InitializeComponent();
+            DataFileChanged += FormMain_DataFileChanged;
         }
 
+        private void FormMain_DataFileChanged(object sender, string e)
+        {
+            label1.Visible = (dataFile != null);
+            label1.Text = e.Substring(e.IndexOf('('), e.Length-e.IndexOf('('));
+            textBox1.Text = e.Substring(0, e.IndexOf('('));
+        }
+
+        ExcelPackage dataFile;
+        public ExcelPackage DataFile
+        {
+            get => dataFile;
+            set
+            {
+                dataFile = value;
+                if (dataFile != null)
+                {
+                    var e = dataFile.File.Name + "(" + dataFile.File.DirectoryName + ")";
+                    DataFileChanged?.Invoke(this, e);
+                }
+            }
+        }
+
+
         FormBankStabibity frmBankStability;
+
+
         private void btnBankStability_Click(object sender, EventArgs e)
         {
-            if (frmBankStability==null||frmBankStability.IsDisposed)
+            if (frmBankStability == null || frmBankStability.IsDisposed)
             {
                 frmBankStability = new FormBankStabibity();
                 frmBankStability.Show();
+            }
+        }
+
+
+        private void btnSelectData_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog()
+            {
+                Filter = "Excel file|*.xlsx; *.xlsm",
+                Multiselect = false,
+                RestoreDirectory = true
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DataFile = new ExcelPackage(new System.IO.FileInfo(ofd.FileName));
             }
         }
     }
